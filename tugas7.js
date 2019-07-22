44dcfd1bd2d94158792d5f15369706c6f3296315
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:53:47 +0700

    add tugas 7 JS

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var i = [145,150,178,170,155,146,172,140,167,165];
+
+// Menggunakan For
+for (var j = 0;j<i.length;j++){
+  console.log(i[j]);
+}
+
+// Menggunakan For OF (tindak menggunakan index)
+for(let x of i){
+  console.log(x);
+}
